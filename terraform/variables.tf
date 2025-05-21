ff6230cbd1619e4cee4739179ac8bceedfe812c9
Swati-cloud <swatizcloud@gmail.com>
2025-05-21 15:49:39 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "mygitkey"
+}
+
+variable "ami_id" {
+  default = "ami-0e35ddab05955cf57"
+}
